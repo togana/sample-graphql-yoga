8ba788dd9a6264233f9de94dad6fa0bcc1f9d884
togana <o.togana@gmail.com>
2024-01-05 16:26:03 +0900

    plugin-with-input を追加した

--- apps/graphql-server/src/builder.ts
+++ apps/graphql-server/src/builder.ts
@@ -2,6 +2,7 @@ import SchemaBuilder from "@pothos/core";
 import PrismaPlugin from '@pothos/plugin-prisma';
 import type PrismaTypes from '@pothos/plugin-prisma/generated';
 import RelayPlugin from "@pothos/plugin-relay";
+import WithInputPlugin from "@pothos/plugin-with-input";
 import { DateTimeResolver } from "graphql-scalars";
 import { prisma, Prisma } from './db';
 
@@ -14,7 +15,7 @@ export const builder = new SchemaBuilder<{
     };
   };
 }>({
-  plugins: [RelayPlugin, PrismaPlugin],
+  plugins: [RelayPlugin, PrismaPlugin, WithInputPlugin],
   relayOptions: {
     cursorType: 'ID',
   },
@@ -26,6 +27,6 @@ export const builder = new SchemaBuilder<{
 });
 
 builder.queryType();
-// builder.mutationType();
+builder.mutationType();
 
 builder.addScalarType("DateTime", DateTimeResolver, {});
