e6fce35ebbc4e3223ce6530716dcf400d64b0492
togana <o.togana@gmail.com>
2024-01-13 15:53:45 +0900

    vite でビルドすると ZodError のインスタンス判定ができなくなるみたいなので tsc でビルドすることにした

--- libs/validator/vite.config.ts
+++ libs/validator/vite.config.ts
@@ -1,19 +1,8 @@
 /// <reference types="vitest" />
 import { defineConfig } from "vite";
-import dts from "vite-plugin-dts";
-import { resolve } from "node:path";
 
 export default defineConfig({
   test: {
     globals: true,
   },
-  build: {
-    lib: {
-      name: "@lib/validator",
-      formats: ["umd"],
-      entry: resolve(__dirname, "src/index.ts"),
-      fileName: "index",
-    },
-  },
-  plugins: [dts()],
 });
